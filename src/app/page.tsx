b9882dc5fac9c8666511bc6b4c74eee960dc85b7
Ittipon787 <aekittipon500@gmail.com>
2024-11-06 09:26:58 +0700

    update logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-// import Image from "next/image";
+import logo from '../app/image/logo01.png';
+import { Card } from "antd";
 
 export default function Home() {
   return (
-    <div style={{backgroundColor:"#ffffff"}}>
-      ระบบปฮิบัติการหัวควย
+    <div style={{ backgroundColor: '#ffffff', textAlign: "center" }}>
+      <Card>
+        <img src={logo.src} alt="Logo" />
+      </Card>
+      <div style={{color:"black"}}>ระบบปฏิบัติการ</div>
+      
     </div>
   );
 }
+
